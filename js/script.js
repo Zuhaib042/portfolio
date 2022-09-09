84f3abf94ce1e6b4d3dec199df71d3f55ffa06f4
Zuhaib Amjad <umar0423@gmail.com>
2022-09-10 03:19:59 +0500

    Add some data in script.js and committing to get back at this point if things go south

--- js/script.js
+++ js/script.js
@@ -24,13 +24,24 @@ links.forEach((link) => {
 // client-side-validation
 const form = document.getElementById('form');
 const message = document.querySelector('.error-mesg');
-const emailValue = document.getElementById('email');
+const email = document.getElementById('email');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (/^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/g.test(emailValue.value)) {
+  if (/^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/g.test(email.value)) {
     form.submit();
     form.reset();
   } else {
     message.innerText = 'Email in lowercase is required';
   }
 });
+
+// local storage
+const nameFull = document.getElementById('user_name').value;
+const valueEmail = email.value;
+const textMessage = document.getElementById('textarea').value;
+
+const object = {name: nameFull, email: valueEmail, message: textMessage};
+const local = localStorage.setItem('data', object);
+function saveData() {}
+
+// const objectStore = JSON.stringify(object);
