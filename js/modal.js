510b2884d4bc261d637760d512648555dc8fba24
Zuhaib Amjad <umar0423@gmail.com>
2022-10-05 12:48:57 +0500

    Fix linters errors

--- js/modal.js
+++ js/modal.js
@@ -47,9 +47,6 @@ const sectionPortfolio = document.querySelector('.works');
 
 function projectsAll() {
   works.forEach((work) => {
-    let techs = work.technology;
-    const items = techs.forEach((item) => console.log(item));
-    // console.log(items);
     const portfolioCard = document.createElement('div');
     portfolioCard.classList.add('skills');
     portfolioCard.innerHTML = `<article class="d-flex flex-column card">
@@ -70,8 +67,8 @@ function projectsAll() {
      </div>
      <ul class="bags d-flex list-none">
        ${work.technology
-         .map((item) => `<li class="tags-li">${item}</li>`)
-         .join('')}  
+    .map((item) => `<li class="tags-li">${item}</li>`)
+    .join('')}  
      </ul>
      <div class="action">
          <button type="button" class="btn project button_project-1" data-tab="1">See Project</button>
@@ -100,8 +97,8 @@ function modalWindow() {
   </div>
   
   <img class="sample mod" src='${
-    work.image
-  }' alt="multi-purpose stories project snapshoot">
+  work.image
+}' alt="multi-purpose stories project snapshoot">
   
   <header class="project-header">
   <p class="project-header-p">${work.description}</p>
@@ -113,11 +110,11 @@ function modalWindow() {
   <div class="rule"></div>
   <div class="btnflex">
   <a href="${
-    work.live
-  }" class="btn mod" type="submit">see live<img class="btn-icon" src="./images/Icon.png" alt="check live version"></a>
+  work.live
+}" class="btn mod" type="submit">see live<img class="btn-icon" src="./images/Icon.png" alt="check live version"></a>
   <a href="${
-    work.repository
-  }" class="btn mod" type="submit">see source<img class="btn-icon" src="./images/github.png" alt="check live version"></a
+  work.repository
+}" class="btn mod" type="submit">see source<img class="btn-icon" src="./images/github.png" alt="check live version"></a
   </div>
   </div>
   
@@ -141,7 +138,7 @@ function eachModal() {
       close.forEach((btn) => {
         btn.addEventListener('click', () => {
           btn.parentElement.parentElement.parentElement.classList.remove(
-            'active'
+            'active',
           );
         });
       });
